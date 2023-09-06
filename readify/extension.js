471e19570fc4c19b5ca0d507cefd25e71ffcc1c9
nataliehill <natalie.a.hill-1@ou.edu>
2023-09-06 10:26:07 -0500

    Functional command to create ReadMe file in directory

--- readify/extension.js
+++ readify/extension.js
@@ -1,33 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "readify" is now active!');
+	let disposable = vscode.commands.registerCommand('readify.Readify', function () {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('readify.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+		const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Readify!');
+		if (workspaceFolder) {
+			const currentDirectory = workspaceFolder.uri.fsPath;
+            const fileName = 'Readme.md';
+            const filePath = path.join(currentDirectory, fileName);
+
+			if (!fs.existsSync(filePath)) {
+                // Create the file
+                fs.writeFileSync(filePath, '', 'utf-8');
+                vscode.window.showInformationMessage(`Created ${fileName} in ${currentDirectory}`);
+            } else {
+                vscode.window.showWarningMessage(`${fileName} already exists in ${currentDirectory}`);
+            }
+        } else {
+            vscode.window.showErrorMessage('No workspace folder opened.');
+        }
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
